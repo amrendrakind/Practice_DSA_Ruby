49a3a9b5f1c246e414b0ae79caa15668e60f04ae
amrendrakind <vision2010@gmail.com>
2022-12-31 22:22:51 +0530

    Hash key value iteration

--- hash.rb
+++ hash.rb
@@ -1,6 +1,11 @@
 months = Hash.new( "month" )
-months = {"1" => "January", "2" => "February"}
+months = {"1" => "January", "2" => "February", "3" => "March", "4" => "April", "5" => "May", "6" => "June" }
 keys = months.keys          # Gives array of keys
 puts "#{keys}"
 values = months.values      # Gives arrays of values
 puts "#{values}"
+
+months.each do |key, value|
+    print key+ " "
+    puts value
+end
