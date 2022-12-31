95c9e40d609803aea05f0b9d45d301823fd28acc
amrendrakind <vision2010@gmail.com>
2022-12-31 22:13:47 +0530

    Hash key values

---
+++ hash.rb
@@ -0,0 +1,6 @@
+months = Hash.new( "month" )
+months = {"1" => "January", "2" => "February"}
+keys = months.keys          # Gives array of keys
+puts "#{keys}"
+values = months.values      # Gives arrays of values
+puts "#{values}"
