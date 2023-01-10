b3f778b067258fa4181fb3f81e9b1dc38e25c016
amrendrakind <vision2010@gmail.com>
2023-01-10 23:06:53 +0530

    Print index of two sum

---
+++ print_indext.rb
@@ -0,0 +1,23 @@
+# nums = [2,7,11,15]
+# target = 9
+# output = [0,1]
+
+# nums = [2,7,11,15]
+nums = [3,2,4]
+
+target = 6
+output = []
+len = nums.length
+i=0
+while(i<len)
+    j=i+1
+    while(j<len)
+        if(nums[i]+nums[j]==target)
+            output.push(i)
+            output.push(j)
+        end
+        j+=1
+    end
+    i+=1
+end
+p output
